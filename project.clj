3c39bd2e3f4a1bf9964490b16bfd3776550f02e4
Micha Niskin <micha.niskin@gmail.com>
2014-09-12 10:51:20 -0400

    working

--- project.clj
+++ project.clj
@@ -9,13 +9,13 @@
   [tailrecursion/boot.notify "2.0.2"]
   [tailrecursion/boot.ring "0.2.1"]]
  :source-paths
- [".boot/tmp/71548/_COLON_tailrecursion.hoplon.boot_SLASH_hoplon_src/dir.tmp"
-  ".boot/tmp/71548/_COLON_tailrecursion.boot.task_SLASH_cljs_out/dir.tmp"
-  ".boot/tmp/71548/_COLON_tailrecursion.hoplon.boot_SLASH_public_out/dir.tmp"
+ [".boot/tmp/71681/_COLON_tailrecursion.hoplon.boot_SLASH_public_out/dir.tmp"
+  ".boot/tmp/71681/_COLON_tailrecursion.hoplon.boot_SLASH_hoplon_src/dir.tmp"
   "src"
-  ".boot/tmp/71548/_COLON_tailrecursion.hoplon.boot_SLASH_hoplon_src_inc_css/dir.tmp"
-  ".boot/tmp/71548/_COLON_tailrecursion.boot.task_SLASH_src_inc_out/dir.tmp"
-  ".boot/tmp/71548/_COLON_tailrecursion.hoplon.boot_SLASH_cljs_out/dir.tmp"
+  ".boot/tmp/71681/_COLON_tailrecursion.hoplon.boot_SLASH_cljs_out/dir.tmp"
+  ".boot/tmp/71681/_COLON_tailrecursion.hoplon.boot_SLASH_hoplon_src_inc_css/dir.tmp"
+  ".boot/tmp/71681/_COLON_tailrecursion.boot.task_SLASH_src_inc_out/dir.tmp"
+  ".boot/tmp/71681/_COLON_tailrecursion.boot.task_SLASH_cljs_out/dir.tmp"
   "resources"]
  :min-lein-version "2.0.0"
  :uberjar-name
